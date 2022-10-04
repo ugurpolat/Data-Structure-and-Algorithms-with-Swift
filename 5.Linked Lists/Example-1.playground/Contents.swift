1ae116f80ee50ffbc005a14f744e6dbc7240c263
Ugur Polat <ugurpolat113@gmail.com>
2022-10-04 09:53:34 +0300

    Add linked list section(examples&questions)

---
+++ 5.Linked Lists/Example-1.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// A linked listis simply a node with a pointer to next
+class Node {
+    var data: Int
+    var next: Node?
+    
+    init(_ data: Int, _ next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+let node3 = Node(3);
+let node2 = Node(2,node3)
+let node1 = Node(1,node2)
+
+// We can print them out by walking the list
+func printLinkedListSimple(_ head: Node?){
+    if head == nil { return }
+    
+    var node = head
+    print(node!.data)
+    
+    while node?.next != nil {
+        print(node!.next!.data)
+        node = node?.next
+    }
+}
+
+printLinkedListSimple(node1)
